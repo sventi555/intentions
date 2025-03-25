5eb1286860d3cb861d811bf44df2cbccae32e246
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-03-25 19:15:27 -0400

    start adding firebase stuff

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,13 +1,30 @@
 import { PageWrapper } from "@/components/page-wrapper";
+import { useState } from "react";
 import { Button, TextInput, View } from "react-native";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [passRepeat, setPassRepeat] = useState("");
+
   return (
     <PageWrapper>
       <View>
-        <TextInput placeholder="email" />
-        <TextInput placeholder="password" />
-        <TextInput placeholder="repeat password" />
+        <TextInput
+          placeholder="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          placeholder="password"
+          value={pass}
+          onChangeText={(text) => setPass(text)}
+        />
+        <TextInput
+          placeholder="repeat password"
+          value={passRepeat}
+          onChangeText={(text) => setPassRepeat(text)}
+        />
         <Button title="create" />
       </View>
     </PageWrapper>
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpIB9oNdO15y23LI0zCld-FPCWIIiHteU",
+  authDomain: "intentions-dbc94.firebaseapp.com",
+  projectId: "intentions-dbc94",
+  storageBucket: "intentions-dbc94.firebasestorage.app",
+  messagingSenderId: "5182096725",
+  appId: "1:5182096725:web:fe065b141e1a16e157dbdc",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
