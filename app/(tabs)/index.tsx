4960a6ec6d3974c7ea2d5d29e9a2f76f77df9904
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-03-28 18:11:38 -0400

    test firestore integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,25 +5,43 @@ import { useUser } from "@/hooks/user";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "expo-router";
 import { signOut } from "firebase/auth";
-import { collection, getDocs } from "firebase/firestore";
-import { ScrollView, Text, View } from "react-native";
+import { collection, doc, getDocs, writeBatch } from "firebase/firestore";
+import { Button, ScrollView, Text, View } from "react-native";
 
 const Feed = () => {
   const user = useUser();
 
-  const { data } = useQuery({
-    queryKey: ["docs"],
-    queryFn: async () => {
-      const query = await getDocs(collection(db, "test"));
-      return query.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-    },
-  });
+  //const { data } = useQuery({
+  //  queryKey: ["docs"],
+  //  queryFn: async () => {
+  //    const query = await getDocs(collection(db, "test"));
+  //    return query.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  //  },
+  //});
 
-  console.log("data", data);
+  //console.log("data", data);
 
   return (
     <View style={{ flex: 1, position: "relative" }}>
       <ScrollView>
+        <Button
+          onPress={() => {
+            if (user) {
+              const batch = writeBatch(db);
+              const userRef = doc(db, "users", user.uid);
+              batch.set(userRef, { username: "sventi555" });
+
+              const usernameRef = doc(db, "index/users/username", "sventi555");
+              batch.set(usernameRef, { userId: user.uid });
+
+              batch
+                .commit()
+                .then(() => console.log("success"))
+                .catch((e) => console.log("failure", e));
+            }
+          }}
+          title="Do the thing"
+        />
         <PageWrapper>
           <View style={{ gap: 8 }}>
             <Post />
