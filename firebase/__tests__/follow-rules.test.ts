7c64712ab97c6a2720bbf783f14e8c67563b5c34
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-04-01 00:07:59 -0400

    a bunch of firebase rule tests

---
+++ firebase/__tests__/follow-rules.test.ts
@@ -0,0 +1,560 @@
+import {
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment,
+  RulesTestContext,
+  RulesTestEnvironment,
+} from "@firebase/rules-unit-testing";
+import {
+  deleteDoc,
+  doc,
+  getDoc,
+  setDoc,
+  setLogLevel,
+  updateDoc,
+} from "firebase/firestore";
+import fs from "node:fs";
+import path from "node:path";
+import { afterAll, beforeAll, beforeEach, describe, it } from "vitest";
+
+setLogLevel("silent");
+
+const USER_IDS = {
+  authUser: "authUser",
+  privateUser: "privateUser",
+  otherPrivateUser: "otherPrivateUser",
+  publicUser: "publicUser",
+  otherPublicUser: "otherPublicUser",
+};
+
+const testUsers = {
+  [USER_IDS.authUser]: { username: "booga", private: true },
+  [USER_IDS.privateUser]: { username: "private-user", private: true },
+  [USER_IDS.otherPrivateUser]: {
+    username: "other-private-user",
+    private: true,
+  },
+  [USER_IDS.publicUser]: { username: "public-user", private: false },
+  [USER_IDS.otherPublicUser]: {
+    username: "other-public-user",
+    private: false,
+  },
+};
+
+const STATUS = { accepted: "accepted", pending: "pending" };
+
+const followDocPath = (fromId: string, toId: string) =>
+  `follows/${fromId}/to/${toId}`;
+
+const addFollowWithoutRules = async (
+  testEnv: RulesTestEnvironment,
+  { from, to, status }: { from: string; to: string; status: string },
+) => {
+  await testEnv.withSecurityRulesDisabled(async (context) => {
+    const db = context.firestore();
+
+    const followDoc = doc(db, followDocPath(from, to));
+    await setDoc(followDoc, { status });
+  });
+};
+
+describe("follow rules", () => {
+  let testEnv: RulesTestEnvironment;
+
+  let authContext: RulesTestContext;
+  let unauthContext: RulesTestContext;
+
+  beforeAll(async () => {
+    testEnv = await initializeTestEnvironment({
+      projectId: "intentions-test",
+      firestore: {
+        rules: fs.readFileSync(
+          path.join(__dirname, "../firestore.rules"),
+          "utf8",
+        ),
+        host: "127.0.0.1",
+        port: 8080,
+      },
+    });
+
+    authContext = testEnv.authenticatedContext(USER_IDS.authUser);
+    unauthContext = testEnv.unauthenticatedContext();
+  });
+
+  beforeEach(async () => {
+    await testEnv.clearFirestore();
+
+    await testEnv.withSecurityRulesDisabled(async (context) => {
+      const db = context.firestore();
+      for (const userId in testUsers) {
+        const userDoc = doc(db, "users", userId);
+        await setDoc(userDoc, testUsers[userId]);
+      }
+    });
+  });
+
+  afterAll(async () => {
+    await testEnv.cleanup();
+  });
+
+  describe("read", () => {
+    // ALLOWED
+    describe.each([
+      {
+        from: USER_IDS.authUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.pending,
+      },
+      {
+        from: USER_IDS.authUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.authUser,
+        status: STATUS.pending,
+      },
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.authUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.authUser,
+        to: USER_IDS.publicUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.publicUser,
+        to: USER_IDS.authUser,
+        status: STATUS.pending,
+      },
+      {
+        from: USER_IDS.publicUser,
+        to: USER_IDS.authUser,
+        status: STATUS.accepted,
+      },
+    ])("includes requester: follow %o", ({ from, to, status }) => {
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should allow reading", async () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        await assertSucceeds(getDoc(followDoc));
+      });
+    });
+
+    describe.each([
+      {
+        from: USER_IDS.publicUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.publicUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.publicUser,
+        to: USER_IDS.otherPublicUser,
+        status: STATUS.accepted,
+      },
+    ])(
+      "includes someone public and is accepted: follow %o",
+      ({ from, to, status }) => {
+        beforeEach(async ({}) => {
+          await addFollowWithoutRules(testEnv, { from, to, status });
+        });
+
+        it("should allow reading", async () => {
+          const db = authContext.firestore();
+
+          const followDoc = doc(db, followDocPath(from, to));
+          await assertSucceeds(getDoc(followDoc));
+        });
+      },
+    );
+
+    describe.each([
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.otherPrivateUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.otherPrivateUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.accepted,
+      },
+    ])(
+      "includes a friend and is accepted: follow %o",
+      ({ from, to, status }) => {
+        beforeEach(async ({}) => {
+          await addFollowWithoutRules(testEnv, { from, to, status });
+          await addFollowWithoutRules(testEnv, {
+            from: USER_IDS.authUser,
+            to: USER_IDS.privateUser,
+            status: STATUS.accepted,
+          });
+        });
+
+        it("should allow reading", async () => {
+          const db = authContext.firestore();
+
+          const followDoc = doc(db, followDocPath(from, to));
+          await assertSucceeds(getDoc(followDoc));
+        });
+      },
+    );
+
+    describe.each([
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.otherPrivateUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.otherPrivateUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.accepted,
+      },
+    ])(
+      "includes a friend (that followed me) and is accepted: follow %o",
+      ({ from, to, status }) => {
+        beforeEach(async ({}) => {
+          await addFollowWithoutRules(testEnv, { from, to, status });
+          await addFollowWithoutRules(testEnv, {
+            from: USER_IDS.privateUser,
+            to: USER_IDS.authUser,
+            status: STATUS.accepted,
+          });
+        });
+
+        it("should allow reading", async () => {
+          const db = authContext.firestore();
+
+          const followDoc = doc(db, followDocPath(from, to));
+          await assertSucceeds(getDoc(followDoc));
+        });
+      },
+    );
+
+    // NOT ALLOWED
+    describe.each([
+      {
+        from: USER_IDS.publicUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.pending,
+      },
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.otherPrivateUser,
+        status: STATUS.pending,
+      },
+    ])(
+      "is pending and does not include requester: follow %o",
+      ({ from, to, status }) => {
+        beforeEach(async ({}) => {
+          await addFollowWithoutRules(testEnv, { from, to, status });
+        });
+
+        it("should not allow reading", async () => {
+          const db = authContext.firestore();
+
+          const followDoc = doc(db, followDocPath(from, to));
+          await assertFails(getDoc(followDoc));
+        });
+      },
+    );
+
+    describe("includes only private non-friends", () => {
+      const { from, to, status } = {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.otherPrivateUser,
+        status: STATUS.accepted,
+      };
+
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should not allow reading", async () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        await assertFails(getDoc(followDoc));
+      });
+    });
+  });
+
+  describe("create", () => {
+    // ALLOWED
+    it("should allow creating accepted follow to public user", async () => {
+      const db = authContext.firestore();
+
+      const followDoc = doc(
+        db,
+        followDocPath(USER_IDS.authUser, USER_IDS.publicUser),
+      );
+      await assertSucceeds(setDoc(followDoc, { status: STATUS.accepted }));
+    });
+
+    it("should allow creating pending follow to private user", async () => {
+      const db = authContext.firestore();
+
+      const followDoc = doc(
+        db,
+        followDocPath(USER_IDS.authUser, USER_IDS.privateUser),
+      );
+      await assertSucceeds(setDoc(followDoc, { status: STATUS.pending }));
+    });
+
+    // NOT ALLOWED
+    it("should not allow creating if user is not signed in", async () => {
+      const db = unauthContext.firestore();
+
+      const followDoc = doc(
+        db,
+        followDocPath(USER_IDS.authUser, USER_IDS.publicUser),
+      );
+      await assertFails(setDoc(followDoc, { status: STATUS.accepted }));
+    });
+
+    it("should not allow creating if fromUserId is not requester", async () => {
+      const db = authContext.firestore();
+
+      const followDoc = doc(
+        db,
+        followDocPath(USER_IDS.publicUser, USER_IDS.otherPublicUser),
+      );
+      await assertFails(setDoc(followDoc, { status: STATUS.accepted }));
+    });
+
+    describe.each([
+      {
+        from: USER_IDS.publicUser,
+        to: USER_IDS.authUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.authUser,
+        status: STATUS.pending,
+      },
+    ])("equivalent follow already exists: %o", ({ from, to, status }) => {
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should not allow creating", async () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(to, from));
+        await assertFails(setDoc(followDoc, { status }));
+      });
+    });
+
+    it("should not allow creating with pending for public user", async () => {
+      const db = authContext.firestore();
+
+      const followDoc = doc(
+        db,
+        followDocPath(USER_IDS.authUser, USER_IDS.publicUser),
+      );
+      await assertFails(setDoc(followDoc, { status: STATUS.pending }));
+    });
+
+    it("should not allow creating with accepted for private user", async () => {
+      const db = authContext.firestore();
+
+      const followDoc = doc(
+        db,
+        followDocPath(USER_IDS.authUser, USER_IDS.privateUser),
+      );
+      await assertFails(setDoc(followDoc, { status: STATUS.accepted }));
+    });
+
+    it("should not allow unknown fields to be included", async () => {
+      const db = authContext.firestore();
+
+      const followDoc = doc(
+        db,
+        followDocPath(USER_IDS.authUser, USER_IDS.privateUser),
+      );
+      await assertFails(
+        setDoc(followDoc, { status: STATUS.pending, extraField: true }),
+      );
+    });
+  });
+
+  describe("update", () => {
+    // ALLOWED
+    describe("private receiver with pending follow", () => {
+      const { from, to, status } = {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.authUser,
+        status: STATUS.pending,
+      };
+
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should allow updating status to success", async () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        await assertSucceeds(updateDoc(followDoc, { status: STATUS.accepted }));
+      });
+    });
+
+    // NOT ALLOWED
+    describe.each([
+      {
+        from: USER_IDS.authUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.pending,
+      },
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.otherPrivateUser,
+        status: STATUS.pending,
+      },
+      {
+        from: USER_IDS.publicUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.pending,
+      },
+    ])("not the receiver: %o", ({ from, to, status }) => {
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should not allow updating status to accepted", async () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        await assertFails(updateDoc(followDoc, { status: STATUS.accepted }));
+      });
+    });
+
+    describe("status is accepted", () => {
+      const { from, to, status } = {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.authUser,
+        status: STATUS.accepted,
+      };
+
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should not allow updating status to pending", async () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        await assertFails(updateDoc(followDoc, { status: STATUS.pending }));
+      });
+    });
+
+    describe("unknown fields are included", () => {
+      const { from, to, status } = {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.authUser,
+        status: STATUS.accepted,
+      };
+
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should not allow updating status to pending", async () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        await assertFails(updateDoc(followDoc, { extraField: true }));
+      });
+    });
+  });
+
+  describe("delete", () => {
+    // ALLOWED
+    describe.each([
+      {
+        from: USER_IDS.authUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.pending,
+      },
+      {
+        from: USER_IDS.authUser,
+        to: USER_IDS.privateUser,
+        status: STATUS.accepted,
+      },
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.authUser,
+        status: STATUS.pending,
+      },
+      {
+        from: USER_IDS.privateUser,
+        to: USER_IDS.authUser,
+        status: STATUS.accepted,
+      },
+    ])("by sender or receiver: %o", ({ from, to, status }) => {
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should allow deleting", async () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        await assertSucceeds(deleteDoc(followDoc));
+      });
+    });
+
+    // NOT ALLOWED
+    describe("not authenticated", () => {
+      const { from, to, status } = {
+        from: USER_IDS.authUser,
+        to: USER_IDS.publicUser,
+        status: STATUS.pending,
+      };
+
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should not allow deleting", async () => {
+        const db = unauthContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        assertFails(deleteDoc(followDoc));
+      });
+    });
+
+    describe("requester not involved", () => {
+      const { from, to, status } = {
+        from: USER_IDS.publicUser,
+        to: USER_IDS.otherPublicUser,
+        status: "accepted",
+      };
+
+      beforeEach(async () => {
+        await addFollowWithoutRules(testEnv, { from, to, status });
+      });
+
+      it("should not allow deleting", () => {
+        const db = authContext.firestore();
+
+        const followDoc = doc(db, followDocPath(from, to));
+        assertFails(deleteDoc(followDoc));
+      });
+    });
+  });
+});
---
+++ firebase/__tests__/user-rules.test.ts
@@ -0,0 +1,242 @@
+import {
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment,
+  RulesTestContext,
+  RulesTestEnvironment,
+} from "@firebase/rules-unit-testing";
+import {
+  deleteDoc,
+  doc,
+  getDoc,
+  setDoc,
+  setLogLevel,
+  updateDoc,
+  writeBatch,
+} from "firebase/firestore";
+import fs from "node:fs";
+import path from "node:path";
+import { afterAll, beforeAll, beforeEach, describe, it } from "vitest";
+
+setLogLevel("silent");
+
+const USER_ID = "USER_ID";
+
+const testUser = {
+  username: "booga",
+  private: true,
+};
+
+const userDocPath = (userId: string) => `users/${userId}`;
+const usernameDocPath = (username: string) =>
+  `index/users/username/${username}`;
+
+describe("user rules", () => {
+  let testEnv: RulesTestEnvironment;
+
+  let authContext: RulesTestContext;
+  let unauthContext: RulesTestContext;
+
+  beforeAll(async () => {
+    testEnv = await initializeTestEnvironment({
+      projectId: "intentions-test",
+      firestore: {
+        rules: fs.readFileSync(
+          path.join(__dirname, "../firestore.rules"),
+          "utf8",
+        ),
+        host: "127.0.0.1",
+        port: 8080,
+      },
+    });
+
+    authContext = testEnv.authenticatedContext(USER_ID);
+    unauthContext = testEnv.unauthenticatedContext();
+  });
+
+  beforeEach(async () => {
+    await testEnv.clearFirestore();
+  });
+
+  afterAll(async () => {
+    await testEnv.cleanup();
+  });
+
+  describe("read", () => {
+    beforeEach(async () => {
+      await testEnv.withSecurityRulesDisabled(async (context) => {
+        const db = context.firestore();
+
+        const userDoc = doc(db, userDocPath(USER_ID));
+        await setDoc(userDoc, testUser);
+
+        const usernameDoc = doc(db, usernameDocPath(testUser.username));
+        await setDoc(usernameDoc, { userId: USER_ID });
+      });
+    });
+
+    it("should allow reading users and usernames", async () => {
+      const db = unauthContext.firestore();
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      assertSucceeds(getDoc(userDoc));
+
+      const usernameDoc = doc(db, usernameDocPath(testUser.username));
+      assertSucceeds(getDoc(usernameDoc));
+    });
+  });
+
+  describe("create", () => {
+    // ALLOWED
+    it("should create user with username", async () => {
+      const db = authContext.firestore();
+      const batch = writeBatch(db);
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      batch.set(userDoc, {
+        ...testUser,
+        imageUrl: "https://example.com",
+      });
+
+      const usernameDoc = doc(db, usernameDocPath(testUser.username));
+      batch.set(usernameDoc, { userId: USER_ID });
+
+      await assertSucceeds(batch.commit());
+    });
+
+    // NOT ALLOWED
+    it("should not allow unknown fields on user", async () => {
+      const db = authContext.firestore();
+      const batch = writeBatch(db);
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      batch.set(userDoc, {
+        ...testUser,
+        extraField: true,
+      });
+
+      const usernameDoc = doc(db, usernameDocPath(testUser.username));
+      batch.set(usernameDoc, { userId: USER_ID });
+
+      await assertFails(batch.commit());
+    });
+
+    it("should not allow unknown fields on username", async () => {
+      const db = authContext.firestore();
+      const batch = writeBatch(db);
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      batch.set(userDoc, testUser);
+
+      const usernameDoc = doc(db, usernameDocPath(testUser.username));
+      batch.set(usernameDoc, { userId: USER_ID, extraField: true });
+
+      await assertFails(batch.commit());
+    });
+
+    it("should not allow creating username or user separately", async () => {
+      const db = authContext.firestore();
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      await assertFails(setDoc(userDoc, testUser));
+
+      const usernameDoc = doc(db, usernameDocPath(testUser.username));
+      await assertFails(setDoc(usernameDoc, { userId: USER_ID }));
+    });
+
+    it("should not allow creating user without authentication", async () => {
+      const db = unauthContext.firestore();
+      const batch = writeBatch(db);
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      batch.set(userDoc, testUser);
+
+      const usernameDoc = doc(db, usernameDocPath(testUser.username));
+      batch.set(usernameDoc, { userId: USER_ID });
+
+      await assertFails(batch.commit());
+    });
+
+    it("should not allow creating user with different id from requester", async () => {
+      const db = authContext.firestore();
+
+      const otherUserId = "DIFFERENT_USER";
+
+      const suspiciousIdPairs = [
+        [USER_ID, otherUserId],
+        [otherUserId, USER_ID],
+        [otherUserId, otherUserId],
+      ];
+
+      for (const idPair of suspiciousIdPairs) {
+        const batch = writeBatch(db);
+        const userDoc = doc(db, userDocPath(idPair[0]));
+        batch.set(userDoc, testUser);
+
+        const usernameDoc = doc(db, usernameDocPath(testUser.username));
+        batch.set(usernameDoc, { userId: idPair[1] });
+
+        await assertFails(batch.commit());
+      }
+    });
+  });
+
+  describe("update", () => {
+    beforeEach(async () => {
+      await testEnv.withSecurityRulesDisabled(async (context) => {
+        const db = context.firestore();
+
+        const userDoc = doc(db, userDocPath(USER_ID));
+        await setDoc(userDoc, testUser);
+
+        const usernameDoc = doc(db, usernameDocPath(testUser.username));
+        await setDoc(usernameDoc, { userId: USER_ID });
+      });
+    });
+
+    // ALLOWED
+    it("should allow updating all fields other than username", async () => {
+      const db = authContext.firestore();
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      await assertSucceeds(
+        updateDoc(userDoc, {
+          private: !testUser.private,
+          imageUrl: "https://new-url.com",
+        }),
+      );
+    });
+
+    // NOT ALLOWED
+    it("should not allow updating user's username", async () => {
+      const db = authContext.firestore();
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      await assertFails(updateDoc(userDoc, { username: "new-username" }));
+    });
+
+    it("should not allow updating username index", async () => {
+      const db = authContext.firestore();
+
+      const usernameDoc = doc(db, usernameDocPath(testUser.username));
+      await assertFails(updateDoc(usernameDoc, { userId: "blah" }));
+    });
+  });
+
+  describe("delete", () => {
+    it("should not allow deleting (for now)", async () => {
+      const db = authContext.firestore();
+
+      const userDoc = doc(db, userDocPath(USER_ID));
+      await assertFails(deleteDoc(userDoc));
+
+      const usernameDoc = doc(db, usernameDocPath(testUser.username));
+      await assertFails(deleteDoc(usernameDoc));
+
+      const batch = writeBatch(db);
+      batch.delete(userDoc);
+      batch.delete(usernameDoc);
+      await assertFails(batch.commit());
+    });
+  });
+});
