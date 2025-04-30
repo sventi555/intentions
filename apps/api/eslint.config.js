0b13112c51affb03488f1fe1d7302587fe834e8c
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-04-30 01:12:01 -0400

    remove eslint

--- apps/api/eslint.config.js
+++
@@ -1,8 +0,0 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.ts'],
-  rules: {},
-});
