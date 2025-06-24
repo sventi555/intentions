9b2701b69e4e03596d295bdef185d39a56fb83b4
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-06-24 11:52:28 -0400

    image downsize and webp

--- apps/api/src/storage.ts
+++ apps/api/src/storage.ts
@@ -1,6 +1,7 @@
 import { HTTPException } from 'hono/http-exception';
 import mime from 'mime-types';
 import path from 'path';
+import sharp from 'sharp';
 import { storage } from './config';
 
 /**
@@ -18,16 +19,19 @@ export const uploadMedia = async (
 
   const [imageMeta, image] = mediaBase64.split(',');
 
+  // i.e. 'image/jpeg'
   const contentType = imageMeta.match(/data:(.*);base64/)?.[1];
   if (!contentType) {
     throw new HTTPException(400, { message: 'missing mime type' });
   }
 
+  // i.e. 'jpeg'
   const extension = mime.extension(contentType);
   if (!extension) {
     throw new HTTPException(400, { message: 'invalid mime type' });
   }
 
+  // i.e. 'image'
   const [mimeType] = contentType.split('/');
   if (!(options.validTypes as string[]).includes(mimeType)) {
     throw new HTTPException(400, {
@@ -38,8 +42,19 @@ export const uploadMedia = async (
   const bucket = storage.bucket();
 
   const imageId = crypto.randomUUID();
-  const imageFilePath = path.join(storageDirName, `${imageId}.${extension}`);
-  await bucket.file(imageFilePath).save(Buffer.from(image, 'base64'));
+  const imageFilePath = path.join(storageDirName, `${imageId}.webp`);
+
+  let resizedImage: Buffer;
+  try {
+    resizedImage = await sharp(Buffer.from(image, 'base64'))
+      .resize(640)
+      .webp()
+      .toBuffer();
+  } catch (err) {
+    throw new HTTPException(400, { message: 'image data is malformed' });
+  }
+
+  await bucket.file(imageFilePath).save(resizedImage);
 
   return imageFilePath;
 };
